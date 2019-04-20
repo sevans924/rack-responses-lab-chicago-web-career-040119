100dc66456ac32fcae465a245b8aa42de3ddb324
Sarah Elzinga <sevans924@gmail.com>
2019-04-20 13:27:17 -0500

    Done.

---
+++ app/application.rb
@@ -0,0 +1,21 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+
+    num_1 = Kernel.rand(1..20)
+    num_2 = Kernel.rand(1..20)
+    num_3 = Kernel.rand(1..20)
+
+    time = Time.now
+
+    if time.hour <= 12
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
+    end
+
+    resp.finish
+  end
+
+end
